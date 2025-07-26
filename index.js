c2cc0ea3d0a1f96717fcfda17e534109a89ad772
NakuRei <nakurei7901@gmail.com>
2025-07-26 15:21:58 +0000

    feat: add try-catch for error handling

--- index.js
+++ index.js
@@ -23,15 +23,19 @@ client.on('voiceStateUpdate', (oldState, newState) => {
     // 入室を検知したボイスチャンネルのIDと一致する場合のみ通知
     const notifyChannel = client.channels.cache.get(notifyTextChannelId);
     if (notifyChannel) {
-      const embed = new EmbedBuilder()
-        .setTitle(
-          `🔔 ${newState.member.displayName} が ` +
-            `VC「${newState.channel.name}」に参加しました`
-        )
-        .setDescription(`<#${newState.channel.id}>`)
-        .setTimestamp()
-        .setColor('#486547');
-      notifyChannel.send({ embeds: [embed] });
+      try {
+        const embed = new EmbedBuilder()
+          .setTitle(
+            `🔔 ${newState.member.displayName} が ` +
+              `VC「${newState.channel.name}」に参加しました`
+          )
+          .setDescription(`<#${newState.channel.id}>`)
+          .setTimestamp()
+          .setColor('#486547');
+        notifyChannel.send({ embeds: [embed] });
+      } catch (error) {
+        console.error('メッセージの送信に失敗しました:', error);
+      }
     }
   }
 });
